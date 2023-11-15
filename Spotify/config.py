53af37bc93635da30acb67ef09d78a7990efe54d
Maria <201903173@alu.comillas.edu>
2023-11-15 18:53:07 +0100

    remove cedentials

--- Spotify/config.py
+++ Spotify/config.py
@@ -1,5 +1,5 @@
-username = 'mariaolivacalero'
-client_id = "1ce8285228d04280ad6184c8cb76b848"
-client_secret = "d78c7903f2d8407da8e9811fa4353e6b"
+username = 'username'
+client_id = "client_id"
+client_secret = "client_secret"
 redirect_uri = "http://localhost:7777/callback"
 scope = 'user-read-recently-played'
